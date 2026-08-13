6f7fbab1f7207d680a70ecd1e9bcee4c4f4f6285
sabujdeep002-bit <sabujdeep002@gmail.com>
2026-08-13 18:03:35 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -3,73 +3,73 @@ window.AGOMONI_SONGS = {
   mahalaya: [
     {
       title: "মহালয়া — বীরেন্দ্রকৃষ্ণ ভদ্র",
-      file:./mahalaya.mp3"
+      file: ./mahalaya.mp3"
     }
   ],
 
   durga: [
     {
       title: "Baja Sanai Aar Baja Re Dhol",
-      file:./Baja-Sanai-Aar-Baja-Re-Dhol.mp3"
+      file: ./Baja-Sanai-Aar-Baja-Re-Dhol.mp3"
     },
     {
       title: "Durga Elo",
-      file:./Durga-Elo.mp3"
+      file: ./Durga-Elo.mp3"
     },
     {
       title: "Dugga Ma",
-      file:./Dugga-Ma.mp3"
+      file: "audio/durga/Dugga-Ma.mp3"
     },
     {
       title: "Ebar Jeno Onno Rokom Pujo",
-      file:./Ebar-Jeno-Onno-Rokom-Pujo.mp3"
+      file: "audio/durga/Ebar-Jeno-Onno-Rokom-Pujo.mp3"
     },
     {
       title: "Shundori Komola",
-      file:./Shundori-Komola.mp3"
+      file: "audio/durga/Shundori-Komola.mp3"
     }
   ],
 
   pandal: [
     {
       title: "Emon Madhur Sandhyay",
-      file:./Emon-Madhur-Sandhyay.mp3"
+      file: "audio/pandal/Emon-Madhur-Sandhyay.mp3"
     },
     {
       title: "Katha Hoyechhilo",
-      file:./Katha-Hoyechhilo.mp3"
+      file: "audio/pandal/Katha-Hoyechhilo.mp3"
     },
     {
       title: "Jete Jete Pathe Holo",
-      file:./Jete-Jete-Pathe-Holo.mp3"
+      file: "audio/pandal/Jete-Jete-Pathe-Holo.mp3"
     },
     {
       title: "Tumi Kato Je Dure",
-      file:./Tumi-Kato-Je-Dure.mp3"
+      file: "audio/pandal/Tumi-Kato-Je-Dure.mp3"
     },
     {
       title: "Phire Ela Dure Giye",
-      file:./Phire-Ela-Dure-Giye.mp3"
+      file: "audio/pandal/Phire-Ela-Dure-Giye.mp3"
     },
     {
       title: "Kotha Kotha Khunjechhi Tomay",
-      file:./Kotha-Kotha-Khunjechhi-Tomay.mp3"
+      file: "audio/pandal/Kotha-Kotha-Khunjechhi-Tomay.mp3"
     },
     {
       title: "Ekta Deshlai Kathi Jwalao",
-      file:./Ekta-Deshlai-Kathi-Jwalao.mp3"
+      file: "audio/pandal/Ekta-Deshlai-Kathi-Jwalao.mp3"
     },
     {
       title: "Mon Bolche Keu Asbe",
-      file:./Mon-Bolche-Keu-Asbe.mp3"
+      file: "audio/pandal/Mon-Bolche-Keu-Asbe.mp3"
     },
     {
       title: "Sandhya Belay Tumi Ami",
-      file:./Sandhya-Belay-Tumi-Ami.mp3"
+      file: "audio/pandal/Sandhya-Belay-Tumi-Ami.mp3"
     },
     {
       title: "Pyar Hamen Kis Mod Pe",
-      file:./Pyar-Hamen-Kis-Mod-Pe.mp3"
+      file: "audio/pandal/Pyar-Hamen-Kis-Mod-Pe.mp3"
     }
   ]
 

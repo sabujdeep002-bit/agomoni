a2dee74bd60ccc646348d5723a121bcb7cc7a407
sabujdeep002-bit <sabujdeep002@gmail.com>
2026-08-13 18:05:55 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -3,73 +3,73 @@ window.AGOMONI_SONGS = {
   mahalaya: [
     {
       title: "মহালয়া — বীরেন্দ্রকৃষ্ণ ভদ্র",
-      file: ./mahalaya.mp3"
+      file: "./mahalaya.mp3"
     }
   ],
 
   durga: [
     {
       title: "Baja Sanai Aar Baja Re Dhol",
-      file: ./Baja Sanai Aar Baja Re Dhol.mp3"
+      file: "./Baja Sanai Aar Baja Re Dhol.mp3"
     },
     {
-      title: "Durga Elo",
-      file: ./Durga Elo.mp3"
+      title: "Dugga Elo",
+      file: "./Dugga Elo.mp3"
     },
     {
       title: "Dugga Ma",
-      file: "audio/durga/Dugga-Ma.mp3"
+      file: "./Dugga Ma.mp3"
     },
     {
       title: "Ebar Jeno Onno Rokom Pujo",
-      file: "audio/durga/Ebar-Jeno-Onno-Rokom-Pujo.mp3"
+      file: "./Ebar Jeno Onno Rokom Puja.mp3"
     },
     {
       title: "Shundori Komola",
-      file: "audio/durga/Shundori-Komola.mp3"
+      file: "./Shundori Komola.mp3"
     }
   ],
 
   pandal: [
     {
       title: "Emon Madhur Sandhyay",
-      file: "audio/pandal/Emon-Madhur-Sandhyay.mp3"
+      file: "./Emon Madhur Sandhyay.mp3"
     },
     {
       title: "Katha Hoyechhilo",
-      file: "audio/pandal/Katha-Hoyechhilo.mp3"
+      file: "./Katha Hoyechhilo.mp3"
     },
     {
       title: "Jete Jete Pathe Holo",
-      file: "audio/pandal/Jete-Jete-Pathe-Holo.mp3"
+      file: "./Jete Jete Pathe Holo.mp3"
     },
     {
       title: "Tumi Kato Je Dure",
-      file: "audio/pandal/Tumi-Kato-Je-Dure.mp3"
+      file: "./Tumi Kato Je Dure.mp3"
     },
     {
-      title: "Phire Ela Dure Giye",
-      file: "audio/pandal/Phire-Ela-Dure-Giye.mp3"
+      title: "Phire Elam Dure Giye",
+      file: "./Phire Elam Dure Giye.mp3"
     },
     {
       title: "Kotha Kotha Khunjechhi Tomay",
-      file: "audio/pandal/Kotha-Kotha-Khunjechhi-Tomay.mp3"
+      file: "./Kotha Kotha Khunjechhi Tomay.mp3"
     },
     {
       title: "Ekta Deshlai Kathi Jwalao",
-      file: "audio/pandal/Ekta-Deshlai-Kathi-Jwalao.mp3"
+      file: "./Ekta Deshlai Kathi Jwalao.mp3"
     },
     {
       title: "Mon Bolche Keu Asbe",
-      file: "audio/pandal/Mon-Bolche-Keu-Asbe.mp3"
+      file: "./Mon Bolche Keu Asbe.mp3"
     },
     {
       title: "Sandhya Belay Tumi Ami",
-      file: "audio/pandal/Sandhya-Belay-Tumi-Ami.mp3"
+      file: "./Sandhya Belay Tumi Ami.mp3"
     },
     {
-      title: "Pyar Hamen Kis Mod Pe",
-      file: "audio/pandal/Pyar-Hamen-Kis-Mod-Pe.mp3"
+      title: "Tumi Kato Je Dure",
+      file: "./Tumi Kato Je Dure.mp3"
     }
   ]
 

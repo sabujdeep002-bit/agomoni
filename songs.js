20ed4c7d8af3e8d9f58dc6ab31c38fa14605f856
sabujdeep002-bit <sabujdeep002@gmail.com>
2026-08-13 18:00:45 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -10,66 +10,66 @@ window.AGOMONI_SONGS = {
   durga: [
     {
       title: "Baja Sanai Aar Baja Re Dhol",
-      file: "/Baja-Sanai-Aar-Baja-Re-Dhol.mp3"
+      file:./Baja-Sanai-Aar-Baja-Re-Dhol.mp3"
     },
     {
       title: "Durga Elo",
-      file: "/Durga-Elo.mp3"
+      file:./Durga-Elo.mp3"
     },
     {
       title: "Dugga Ma",
-      file: "/Dugga-Ma.mp3"
+      file:./Dugga-Ma.mp3"
     },
     {
       title: "Ebar Jeno Onno Rokom Pujo",
-      file: "/Ebar-Jeno-Onno-Rokom-Pujo.mp3"
+      file:./Ebar-Jeno-Onno-Rokom-Pujo.mp3"
     },
     {
       title: "Shundori Komola",
-      file: "/Shundori-Komola.mp3"
+      file:./Shundori-Komola.mp3"
     }
   ],
 
   pandal: [
     {
       title: "Emon Madhur Sandhyay",
-      file: "/Emon-Madhur-Sandhyay.mp3"
+      file:./Emon-Madhur-Sandhyay.mp3"
     },
     {
       title: "Katha Hoyechhilo",
-      file: "/Katha-Hoyechhilo.mp3"
+      file:./Katha-Hoyechhilo.mp3"
     },
     {
       title: "Jete Jete Pathe Holo",
-      file: "/Jete-Jete-Pathe-Holo.mp3"
+      file:./Jete-Jete-Pathe-Holo.mp3"
     },
     {
       title: "Tumi Kato Je Dure",
-      file: "/Tumi-Kato-Je-Dure.mp3"
+      file:./Tumi-Kato-Je-Dure.mp3"
     },
     {
       title: "Phire Ela Dure Giye",
-      file: "/Phire-Ela-Dure-Giye.mp3"
+      file:./Phire-Ela-Dure-Giye.mp3"
     },
     {
       title: "Kotha Kotha Khunjechhi Tomay",
-      file: "/Kotha-Kotha-Khunjechhi-Tomay.mp3"
+      file:./Kotha-Kotha-Khunjechhi-Tomay.mp3"
     },
     {
       title: "Ekta Deshlai Kathi Jwalao",
-      file: "/Ekta-Deshlai-Kathi-Jwalao.mp3"
+      file:./Ekta-Deshlai-Kathi-Jwalao.mp3"
     },
     {
       title: "Mon Bolche Keu Asbe",
-      file: "/Mon-Bolche-Keu-Asbe.mp3"
+      file:./Mon-Bolche-Keu-Asbe.mp3"
     },
     {
       title: "Sandhya Belay Tumi Ami",
-      file: "/Sandhya-Belay-Tumi-Ami.mp3"
+      file:./Sandhya-Belay-Tumi-Ami.mp3"
     },
     {
       title: "Pyar Hamen Kis Mod Pe",
-      file: "/Pyar-Hamen-Kis-Mod-Pe.mp3"
+      file:./Pyar-Hamen-Kis-Mod-Pe.mp3"
     }
   ]
 

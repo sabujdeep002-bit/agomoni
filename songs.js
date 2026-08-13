8ef36ba46844e0e07d3802e34196bc0c5e0939b7
sabujdeep002-bit <sabujdeep002@gmail.com>
2026-08-13 18:07:01 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -3,18 +3,18 @@ window.AGOMONI_SONGS = {
   mahalaya: [
     {
       title: "মহালয়া — বীরেন্দ্রকৃষ্ণ ভদ্র",
-      file: "./mahalaya.mp3"
+      file: ./mahalaya.mp3"
     }
   ],
 
   durga: [
     {
       title: "Baja Sanai Aar Baja Re Dhol",
-      file: "./Baja Sanai Aar Baja Re Dhol.mp3"
+      file: ./Baja Sanai Aar Baja Re Dhol.mp3"
     },
     {
       title: "Dugga Elo",
-      file: "./Dugga Elo.mp3"
+      file: ./Dugga Elo.mp3"
     },
     {
       title: "Dugga Ma",

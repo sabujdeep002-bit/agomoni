c10a79c6d85c0ee15a3e5d2d410e611a90ee9321
sabujdeep002-bit <sabujdeep002@gmail.com>
2026-08-13 17:57:07 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,24 +1,76 @@
 window.AGOMONI_SONGS = {
+
   mahalaya: [
-    { title: 'মহালয়া — বীরেন্দ্রকৃষ্ণ ভদ্র', file:',/mahalaya.mp3' }
+    {
+      title: "মহালয়া — বীরেন্দ্রকৃষ্ণ ভদ্র",
+      file: "./mahalaya.mp3"
+    }
   ],
+
   durga: [
-    { title: 'Baja Sanai Aar Baja Re Dhol', file:',/Baja Sanai Aar Baja Re Dhol.mp3' },
-    { title: 'Dugga Elo', file: 'audio/দুর্গাপূজা%20OGs/Dugga%20Elo.mp3' },
-    { title: 'Dugga Ma', file: 'audio/দুর্গাপূজা%20OGs/Dugga%20Ma.mp3' },
-    { title: 'Ebar Jeno Onno Rokom Pujo', file: 'audio/দুর্গাপূজা%20OGs/Ebar%20Jeno%20Onno%20Rokom%20Pujo.mp3' },
-    { title: 'Shundori Komola', file: 'audio/দুর্গাপূজা%20OGs/Shundori%20Komola.mp3' }
+    {
+      title: "Baja Sanai Aar Baja Re Dhol",
+      file: "/Baja-Sanai-Aar-Baja-Re-Dhol.mp3"
+    },
+    {
+      title: "Durga Elo",
+      file: "/Durga-Elo.mp3"
+    },
+    {
+      title: "Dugga Ma",
+      file: "/Dugga-Ma.mp3"
+    },
+    {
+      title: "Ebar Jeno Onno Rokom Pujo",
+      file: "/Ebar-Jeno-Onno-Rokom-Pujo.mp3"
+    },
+    {
+      title: "Shundori Komola",
+      file: "/Shundori-Komola.mp3"
+    }
   ],
+
   pandal: [
-    { title: 'Emon Madhur Sandhyay', file: 'audio/প্যান্ডেল%20vibe/Emon%20Madhur%20Sandhyay.mp3' },
-    { title: 'Katha Hoyechhilo', file: 'audio/প্যান্ডেল%20vibe/Katha%20Hoyechhilo.mp3' },
-    { title: 'Jete Jete Pathe Holo', file: 'audio/প্যান্ডেল%20vibe/Jete%20Jete%20Pathe%20Holo.mp3' },
-    { title: 'Tumi Kato Je Dure', file: 'audio/প্যান্ডেল%20vibe/Tumi%20Kato%20Je%20Dure.mp3' },
-    { title: 'Phire Elam Dure Giye', file: 'audio/প্যান্ডেল%20vibe/Phire%20Elam%20Dure%20Giye.mp3' },
-    { title: 'Kotha Kotha Khunjechhi Tomay', file: 'audio/প্যান্ডেল%20vibe/Kotha%20Kotha%20Khunjechhi%20Tomay.mp3' },
-    { title: 'Ekta Deshlai Kathi Jwalao', file: 'audio/প্যান্ডেল%20vibe/Ekta%20Deshlai%20Kathi%20Jwalao.mp3' },
-    { title: 'Mon Bolche Keu Asbe', file: 'audio/প্যান্ডেল%20vibe/Mon%20Bolche%20Keu%20Asbe.mp3' },
-    { title: 'Sandhya Belay Tumi Ami', file: 'audio/প্যান্ডেল%20vibe/Sandhya%20Belay%20Tumi%20Ami.mp3' },
-    { title: 'Pyar Hamen Kis Mod Pe - From Satte Pe Satta', file: 'audio/প্যান্ডেল%20vibe/Pyar%20Hamen%20Kis%20Mod%20Pe%20-%20From%20Satte%20Pe%20Satta.mp3' }
+    {
+      title: "Emon Madhur Sandhyay",
+      file: "/Emon-Madhur-Sandhyay.mp3"
+    },
+    {
+      title: "Katha Hoyechhilo",
+      file: "/Katha-Hoyechhilo.mp3"
+    },
+    {
+      title: "Jete Jete Pathe Holo",
+      file: "/Jete-Jete-Pathe-Holo.mp3"
+    },
+    {
+      title: "Tumi Kato Je Dure",
+      file: "/Tumi-Kato-Je-Dure.mp3"
+    },
+    {
+      title: "Phire Ela Dure Giye",
+      file: "/Phire-Ela-Dure-Giye.mp3"
+    },
+    {
+      title: "Kotha Kotha Khunjechhi Tomay",
+      file: "/Kotha-Kotha-Khunjechhi-Tomay.mp3"
+    },
+    {
+      title: "Ekta Deshlai Kathi Jwalao",
+      file: "/Ekta-Deshlai-Kathi-Jwalao.mp3"
+    },
+    {
+      title: "Mon Bolche Keu Asbe",
+      file: "/Mon-Bolche-Keu-Asbe.mp3"
+    },
+    {
+      title: "Sandhya Belay Tumi Ami",
+      file: "/Sandhya-Belay-Tumi-Ami.mp3"
+    },
+    {
+      title: "Pyar Hamen Kis Mod Pe",
+      file: "/Pyar-Hamen-Kis-Mod-Pe.mp3"
+    }
   ]
+
 };

6b4b95480755c182bbfc3776c3828399f4e03f39
sabujdeep002-bit <sabujdeep002@gmail.com>
2026-08-14 21:51:21 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -12,66 +12,99 @@ window.AGOMONI_SONGS = {
       title: "Baja Sanai Aar Baja Re Dhol",
       file: "./Baja Sanai Aar Baja Re Dhol.mp3"
     },
+
     {
-      title: "Dugga Elo",
-      file: "./Dugga Elo.mp3"
+      title: "Durga Elo",
+      file: "./Durga Elo.mp3"
     },
+
     {
-      title: "Dugga Ma",
-      file: "./Dugga Ma.mp3"
+      title: "Durga Ma",
+      file: "./Durga Ma.mp3"
     },
+
     {
       title: "Ebar Jeno Onno Rokom Pujo",
       file: "./Ebar Jeno Onno Rokom Pujo.mp3"
     },
+
     {
       title: "Shundori Komola",
       file: "./Shundori Komola.mp3"
-    }
     },
+
     {
-      title: "Aaj Baaje_spotdown.org.mp3",
+      title: "Aaj Baaje",
       file: "./ss/Aaj Baaje_spotdown.org.mp3"
+    },
+
+    {
+      title: "Aambar Dugga",
+      file: "./ss/Aambar Dugga_spotdown.org.mp3"
+    },
+
+    {
+      title: "Aigiri Nandini - Rock Version",
+      file: "./ss/Aigiri Nandini - Rock Version_spotdown.org.mp3"
+    },
+
+    {
+      title: "Ailo Uma Barite",
+      file: "./ss/Ailo Uma Barite_spotdown.org.mp3"
     }
   ],
 
-pandal: [
+  pandal: [
     {
       title: "Ekta Deshlai Kathi Jwalao",
       file: "./Ekta Deshlai Kathi Jwalao.mp3"
     },
+
     {
       title: "Emon Madhur Sandhyay",
       file: "./Emon Madhur Sandhyay.mp3"
     },
+
     {
       title: "Jete Jete Pathe Holo",
       file: "./Jete Jete Pathe Holo.mp3"
     },
+
     {
       title: "Katha Hoyechhilo",
       file: "./Katha Hoyechhilo.mp3"
     },
+
     {
       title: "Kotha Kotha Khunjechhi Tomay",
       file: "./Kotha Kotha Khunjechhi Tomay.mp3"
     },
+
     {
       title: "Mon Bolche Keu Asbe",
       file: "./Mon Bolche Keu Asbe.mp3"
     },
+
     {
       title: "Phire Elam Dure Giye",
       file: "./Phire Elam Dure Giye.mp3"
     },
+
     {
-      title: "Pyar Hamen Kis Mod Pe - From Satte Pe Satta",
-      file: "./Pyar Hamen Kis Mod Pe - From Satte Pe Satta.mp3"
+      title: "Pyar Hamen Kis Mod Pe - From Sattе Pe Satta",
+      file: "./Pyar Hamen Kis Mod Pe - From Sattе Pe Satta.mp3"
     },
+
     {
       title: "Sandhya Belay Tumi Ami",
       file: "./Sandhya Belay Tumi Ami.mp3"
     },
+
+    {
+      title: "Shundori Komola",
+      file: "./Shundori Komola.mp3"
+    },
+
     {
       title: "Tumi Kato Je Dure",
       file: "./Tumi Kato Je Dure.mp3"
